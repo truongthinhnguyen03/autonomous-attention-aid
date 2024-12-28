66a929670cfbf16f373c8d24f89a21fec200eb94
truongthinhnguyen03 <truongthinh.nguyen03@gmail.com>
2024-12-28 15:14:55 +0700

    implement basic extension popup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<svelte:head>
+    <link rel="stylesheet" href="popup.css">
+</svelte:head>
+
+<div class="flex flex-col items-center justify-center h-full">
+    <h1 class="text-2xl font-bold p-4">Autonomous Attention Aid</h1>
+    <div class="flex flex-row items-center justify-center gap-4">
+        <button class="bg-blue-500 text-white px-4 py-2 rounded-md">Connect device</button>
+        <button class="bg-blue-500 text-white px-4 py-2 rounded-md">Start session</button>
+    </div>
+</div>
